f3144ce5bb253e7bb26249ef5bfe632d6597f640
tananin <tananin@mail.ru>
2015-05-25 17:47:47 +0700

    2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # html5
-HTML5
+
+1. Клонируем пакет нового сайта с GITHAB: git clone https://github.com/tananin/html5.git
+2. Качаем нужные пакеты: bower update
+3. Создаём новый проект Compass: compass init
+4. Запускаем compass: compass watch
--- config.rb
+++ config.rb
@@ -1,12 +1,19 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
+add_import_path "libs/normalize.scss"
+add_import_path "libs/fontawesome/scss"
+
+add_import_path "sass/components/"
+
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
