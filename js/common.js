1d137fd0bd106c1277bd08480a3f11c2ec1c88e1
Maxim Tananin <tananin@mail.ru>
2016-08-07 23:18:08 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -37,6 +37,11 @@ $(document).ready(function() {
 		return false;
 	});
 
-
+	//Медленная прокрутка до якоря
+	$("body").on('click', '[href*="#"]', function(e){
+	    var fixed_offset = 100;
+	    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+	    e.preventDefault();
+	});
 
 });	
